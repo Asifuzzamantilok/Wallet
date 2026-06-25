ae5f9971b9367d0cb131e11614c0b253372a37d7
Asifuzzamantilok <124570759+Asifuzzamantilok@users.noreply.github.com>
2026-06-25 19:57:07 +0600

    Add files via upload

---
+++ lib/core/constants/colours.dart
@@ -0,0 +1,13 @@
+1import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryStart = Color(0xFF5B2EFF);
+  static const primaryEnd = Color(0xFF00A8FF);
+
+  static const background = Color(0xFFF8F9FB);
+
+  static const success = Color(0xFF22C55E);
+  static const error = Color(0xFFEF4444);
+
+  static const textPrimary = Color(0xFF0F172A);
+}
